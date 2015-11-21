2787e72b130c5d1e71fa50fb4654757081f935c9
sagorti <sagorti@SAGORTI-WS.cisco.com>
2015-11-20 17:01:49 -0800

    Add new activity

--- MyApplication/app/src/main/java/cisco/sagorti/myfirstapp/DisplayMessageActivity.java
+++ MyApplication/app/src/main/java/cisco/sagorti/myfirstapp/DisplayMessageActivity.java
@@ -5,25 +5,23 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class MyActivity extends ActionBarActivity {
-
-    public static final String EXTRA_MESSAGE = "com.cisco.myfirstapp.MESSAGE";
+public class DisplayMessageActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_my, menu);
-        return true;
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        
+        setContentView(textView);
     }
 
     @Override
@@ -39,13 +37,5 @@ public class MyActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
     }
 }
